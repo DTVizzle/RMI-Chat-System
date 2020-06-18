eda79e0db3296655569c7e1291d2730dead52ec1
Sinan Rassam <sinan.rassam@outlook.com>
2020-06-19 08:53:02 +1200

    Made all clients aware of each other

--- src/ChatInterface.java
+++ src/ChatInterface.java
@@ -2,6 +2,7 @@
 import java.rmi.Remote;
 import java.rmi.RemoteException;
 import java.util.ArrayList;
+import javax.swing.DefaultListModel;
 import javax.swing.JTextArea;
 
 /*
@@ -18,6 +19,8 @@ public interface ChatInterface extends Remote {
     public void setName(String name) throws RemoteException;
     
     public void setMessageArea(JTextArea messageArea) throws RemoteException;
+    
+    public void setClientsArea(DefaultListModel clientsModel) throws RemoteException;
 
     public String getName() throws RemoteException;
 
@@ -26,4 +29,6 @@ public interface ChatInterface extends Remote {
     public void addClient(ChatInterface client) throws RemoteException;
 
     public ArrayList<ChatInterface> getClients() throws RemoteException;
+    
+    public void getClientsFromHost(ChatInterface host) throws RemoteException;
 }
